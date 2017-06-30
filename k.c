43a241fc35fb08259bfd6bc4c61abbc4c8bf9fa9
azhagumanikandan <azhagumanikandan452000@gmail.com>
2017-06-30 14:01:05 +0530

    Update k.c

--- k.c
+++ k.c
@@ -1,1 +1,19 @@
 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int year,n;
+clrscr();
+printf("\n enter the year");
+scanf("%d",&year);
+if((n%400==0)||(n%100==0)||(n%4==0))
+{
+printf("\n the year is not leap year");
+}
+else
+{
+printf("\n the year is leap year");
+}
+getch();
+}
